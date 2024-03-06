1fce4e37777a0e9fe73c672612b27edef106721c
NickGolovanov <144025505+NickGolovanov@users.noreply.github.com>
2024-03-06 14:44:27 +0100

    Update testingCode2.ino

--- ProjectArduinoAssignments/MainCode/Main/testingCode2.ino
+++ ProjectArduinoAssignments/MainCode/Main/testingCode2.ino
@@ -2,11 +2,20 @@ const int motorA1 = 5;
 const int motorA2 = 4;
 const int motorB1 = 3;
 const int motorB2 = 2;
+const int EchoPin = 12;
+const int TrigerPin = 13;
+const int Gripper = 8;
 
-int sensorValues[] = {0,0,0,0,0,0,0,0};
-int sensorPins[] = {A0,A1,A2,A3,A4,A5,A6,A7};
+int sensorValues[8];
+int sensorPins[] = {A7, A6, A5, A4, A3, A2, A1, A0};
+
+bool wait = true;
+bool set = false;
+bool solved = false;
+bool victory = false;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(motorA1, OUTPUT);
   pinMode(motorA2, OUTPUT);
   pinMode(motorB1, OUTPUT);
@@ -19,100 +28,109 @@ void setup() {
   pinMode(A5, INPUT);
   pinMode(A6, INPUT);
   pinMode(A7, INPUT);
+
+  pinMode(EchoPin, INPUT);
+  pinMode(TrigerPin, OUTPUT);
+  pinMode(Gripper, OUTPUT);
 }
+
+
 
 void loop() {
-  getSensor();
-  
-//  if (sensorValues[3] == 1 || sensorValues[4] == 1) {
-//    forward();
-//  }
-//  else if ((sensorValues[0] == 1 && sensorValues[1] == 1 && sensorValues[2] == 1) || (sensorValues[0] == 1 && sensorValues[1] == 1)){
-//    right();
-//  }
-  if ((sensorValues[5] == 1 && sensorValues[6] == 1 && sensorValues[7] == 1) || (sensorValues[6] == 1 && sensorValues[7] == 1)){
-    left();
-  }
-  else if (sensorValues[0] == 0 && sensorValues[1] == 0 && sensorValues[2] == 0 && sensorValues[3] == 0 && 
-        sensorValues[4] == 0 && sensorValues[5] == 0 && sensorValues[6] == 0 && sensorValues[7] == 0) {
-      // If all sensors are off the line, turn around
-      turnAround();
+    
+    if (wait)
+    {
+       detectObject();
     }
-  else if (sensorValues[2] == 1) {
-    adjustRight();
-  }  
-  else if (sensorValues[5] == 1) {
-    adjustLeft();
-  }
-  else {
-    forward();
-  }
+    else if (!set)
+    {
+      entering();
+    }
+   else if (!solved)
+   {
+     maze();
+   }
+   else if(!victory)
+   {
+     finish();
+   }
 }
 
 
-void getSensor(){
-   for(int i = 0; i < sizeof(sensorPins) / sizeof(sensorPins[0]); i++) {
+
+
+//================================================================================================//
+void detectObject()
+{
+  int distance = getUltrasonicDistance();
+
+  if(distance < 25)
+  {
+    wait = false;
+  } 
+}
+//=========================================== CODE FOR THE GRABING THE BLOCK AND CALIBRATION ===================================================//
+void entering()
+{
+
+        if (sensorValues[0] == 0 && sensorValues[1] == 0 && sensorValues[2] == 0 && sensorValues[3] == 0 && sensorValues[4] == 0 && sensorValues[5] == 0 && sensorValues[6] == 0 && sensorValues[7] == 0){
+            forward();
+        }
+        else if (sensorValues[0] == 1 && sensorValues[1] == 1 && sensorValues[2] == 1 && sensorValues[3] == 1 && sensorValues[4] == 1 && sensorValues[5] == 1 && sensorValues[6] == 1 && sensorValues[7] == 1){
+            forward();
+        }
+//    else 
+    {
+//      stop();  
+//    }
+
+  set = true;
+}
+//================================================================================================//
+//void maze()
+//{
+//  
+//}
+//================================================================================================//
+//void finish(
+//================================================================================================//
+void activateGripper(int angle) {
+  int pulseWidth = map(angle, 0, 180, 0, 255);
+  digitalWrite(Gripper, HIGH);
+  Serial.println(pulseWidth);
+  delayMicroseconds(pulseWidth);
+  digitalWrite(Gripper, LOW);
+  delay(20);
+}
+//================================================================================================//
+void getSensor() {
+  for (int i = 0; i < 8; i++) {
     int sensorState = analogRead(sensorPins[i]);
     sensorValues[i] = sensorState >= 800 ? 1 : 0;
 
     Serial.print(sensorValues[i]);
     Serial.print(" ");
-    
-   }
-   Serial.println("");
+  }
+  Serial.println("");
+}
+//================================================================================================//
+void forward() {
+  analogWrite(motorA1, 200);
+  analogWrite(motorA2, 0);
+  analogWrite(motorB1, 200);
+  analogWrite(motorB2, 0);
 }
 
-
-void forward(){
-    analogWrite(motorA1, 200); //480
-    analogWrite(motorA2, 0);
-    analogWrite(motorB1, 200); //474
-    analogWrite(motorB2, 0);
- }
-void backward(){
-
-    analogWrite(motorA1, 0);
-    analogWrite(motorA2, 200); // 480
-    analogWrite(motorB1, 0);
-    analogWrite(motorB2, 200); // 474
-
+void backward() {
+  analogWrite(motorA1, 0);
+  analogWrite(motorA2, 200);
+  analogWrite(motorB1, 0);
+  analogWrite(motorB2, 200);
 }
+
 void stop() {
-    analogWrite(motorA1, 0);
-    analogWrite(motorA2, 0);
-    analogWrite(motorB1, 0);
-    analogWrite(motorB2, 0);
-}
-
-void left() {
-    analogWrite(motorA1, 0);
-    analogWrite(motorA2, 200);
-    analogWrite(motorB1, 200);
-    analogWrite(motorB2, 0);
-}
-
-void right() {
-    analogWrite(motorA1, 100);
-    analogWrite(motorA2, 0);
-    analogWrite(motorB1, 0);
-    analogWrite(motorB2, 100);
-}
-
-void adjustLeft() {
-    analogWrite(motorA1, 0);
-    analogWrite(motorA2, 100);
-    analogWrite(motorB1, 150);
-    analogWrite(motorB2, 0);
-}
-void adjustRight(){
-    analogWrite(motorA1, 150);
-    analogWrite(motorA2, 0);
-    analogWrite(motorB1, 0);
-    analogWrite(motorB2, 100);
-}
-void turnAround(){
-    analogWrite(motorA1, 150);
-    analogWrite(motorA2, 0);
-    analogWrite(motorB1, 0);
-    analogWrite(motorB2, 150);
+  analogWrite(motorA1, 0);
+  analogWrite(motorA2, 0);
+  analogWrite(motorB1, 0);
+  analogWrite(motorB2, 0);
 }
