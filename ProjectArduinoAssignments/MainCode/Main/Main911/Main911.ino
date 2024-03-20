2c6b6435f5bf352dee34896805de45596febb1a5
NickGolovanov <nikita.golovanov@student.nhlstenden.com>
2024-03-20 14:02:40 +0100

    new changes

--- ProjectArduinoAssignments/MainCode/Main/Main911/Main911.ino
+++ ProjectArduinoAssignments/MainCode/Main/Main911/Main911.ino
@@ -12,18 +12,19 @@ const int Gripper = 12;
 #define GRIPPER_CLOSE 950
 #define GRIPPER_OPEN 1600
 
-int sensorValues[] = {0, 0, 0, 0, 0, 0, 0, 0};
-int sensorPins[] = {A0, A1, A2, A3, A4, A5, A6, A7};
+int sensorValues[8];
+int sensorPins[] = {A7, A6, A5, A4, A3, A2, A1, A0};
+
+volatile int L = 0;
+volatile int R = 0;
 
 bool check = true;
 bool first = false;
 bool second = false;
 bool success = false;
 
-volatile int L = 0;
-volatile int R = 0;
-
 void setup() {
+  Serial.begin(9600);
   pinMode(motorA1, OUTPUT);
   pinMode(motorA2, OUTPUT);
   pinMode(motorB1, OUTPUT);
@@ -45,34 +46,40 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(MotorR2), ISR_R, CHANGE);
 }
 
+
+
 void loop() {
-
-  if (check) {
-    detec();
-  }
-  else if (!first) {
-    enter();
-  }
-  else if (!second) {
-    maze();
-  }
-  else if (!success) {
-    finish();
-  }
-}
-
-void finish() {
-      stop();
-      activateGripper(1700);
-      delay(500);
-      back(20);
-      stop();
-
-  success = true;
-
+    
+    if (check)
+    {
+       detectObject();
+    }
+    else if (!first)
+    {
+      enter();
+    }
+   else if (!second)
+   {
+     maze();
+   }
+   else if(!success)
+   {
+     finish();
+   }
 }
 
 
+//===========================================CHECK THE OBJECT===================================================//
+void detectObject()
+{
+  int distance = getUltrasonicDistance();
+
+  if(distance < 25)
+  {
+    check = false;
+  } 
+}
+//===========================================START===================================================//
 void enter() {
   delay(1000);
 
@@ -110,6 +117,7 @@ void enter() {
   }
   first = true;
 }
+//===========================================MAZE===================================================//
 
 void maze() {
   getSensor();
@@ -123,35 +131,19 @@ void maze() {
 
       if(sensorValues[0] == 1 && sensorValues[1] == 1 && sensorValues[2] == 1 && sensorValues[3] == 1 &&
          sensorValues[4] == 1 && sensorValues[5] == 1 && sensorValues[6] == 1 && sensorValues[7] == 1)
-         {
+       {
             second = true;
-         }
+       }
        else 
        {        
-          turnLeft(35);
+          turnLeft(37);
           delay(100);
        }
-
-    
   }
   else if (sensorValues[0] == 0 && sensorValues[1] == 0 && sensorValues[2] == 0 && sensorValues[3] == 0 &&
            sensorValues[4] == 0 && sensorValues[5] == 0 && sensorValues[6] == 0 && sensorValues[7] == 0) {
     turnAround();
   }
-//    else if (sensorValues[0] == 1 && sensorValues[1] == 1 && sensorValues[2] == 1 && sensorValues[3] == 1 &&
-//             sensorValues[4] == 1 && sensorValues[5] == 1 && sensorValues[6] == 1 && sensorValues[7] == 1) {
-//      delay(300);
-//      forward();
-//      stop();
-//      getSensor();
-//      delay(500);
-//
-//      if(sensorValues[0] == 1 && sensorValues[1] == 1 && sensorValues[2] == 1 && sensorValues[3] == 1 &&
-//         sensorValues[4] == 1 && sensorValues[5] == 1 && sensorValues[6] == 1 && sensorValues[7] == 1)
-//         {
-//            second = true;
-//         }
-//  }
   else if (sensorValues[2] == 1) {
     adjustRight();
   }
@@ -162,19 +154,32 @@ void maze() {
     forward();
   }
 }
+//===========================================FINISH===================================================//
+
+void finish() {
+      stop();
+      activateGripper(1700);
+      delay(500);
+      back(20);
+      stop();
+
+  success = true;
+
+}
+//===========================================SENSOR===================================================//
 
 
 void getSensor() {
-  for (int i = 0; i < sizeof(sensorPins) / sizeof(sensorPins[0]); i++) {
+  for (int i = 0; i < 8; i++) {
     int sensorState = analogRead(sensorPins[i]);
     sensorValues[i] = sensorState >= 800 ? 1 : 0;
 
     Serial.print(sensorValues[i]);
     Serial.print(" ");
-
   }
   Serial.println("");
 }
+//===============================================TURN LEFT, RIGHT, BACK [R-Motor]=================================================//
 
 void ISR_L() {
   L++;
@@ -183,12 +188,12 @@ void ISR_L() {
 void ISR_R() {
   R++;
 }
-
-void turnLeft(int d) {
-  L = 0;
-  R = 0;
-
-  while (L < d) {
+//=-=-=-=-=-=-=-=-=-=-//
+void turnLeft(int d){
+  L=0;
+  R=0;
+  
+  while(L < d){
     digitalWrite(motorA1, LOW); //480
     digitalWrite(motorA2, LOW);
     digitalWrite(motorB1, HIGH); //474
@@ -198,23 +203,22 @@ void turnLeft(int d) {
   }
   stop();
 }
-
-void turnRight(int d) {
-  L = 0;
-  R = 0;
-
-  while (R < d) {
-    digitalWrite(motorA1, HIGH); //480
-    digitalWrite(motorA2, LOW);
-    digitalWrite(motorB1, LOW); //474
-    digitalWrite(motorB2, LOW);
+//=-=-=-=-=-=-=-=-=-=-//
+void turnRight(int d){
+  L=0;
+  R=0;
+  
+  while(R < d){
+    digitalWrite(motorA1, 0); //480
+    digitalWrite(motorA2, 0);
+    digitalWrite(motorB1, 0); //474
+    digitalWrite(motorB2, 0);
 
     Serial.println(R);
   }
   stop();
 }
-
-
+//=-=-=-=-=-=-=-=-=-=-//
 void back(int d) {
   L = 0;
   R = 0;
@@ -227,26 +231,52 @@ void back(int d) {
   }
   stop();
 }
+//===============================================GRIPPER=================================================//
+void activateGripper(int pulse) {
+  for (int i = 0; i < 10; i++)
+  {
+    digitalWrite(Gripper, HIGH);
+    delayMicroseconds(pulse);
+    digitalWrite(Gripper, LOW);
+    delay(20);
+  }
+}
+//===============================================DISTANCE=================================================//
+
+int getUltrasonicDistance() {
+  digitalWrite(TrigerPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(TrigerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TrigerPin, LOW);
+
+  long duration = pulseIn(EchoPin, HIGH);
+  int distance = duration * 0.034 / 2;
+
+  return distance;
+}
+//===============================================MOTTOR A1-A2-B1-B2=================================================//
+
 void forward() {
-  digitalWrite(motorA1, HIGH); //480
+  digitalWrite(motorA1, HIGH);
   digitalWrite(motorA2, LOW);
-  digitalWrite(motorB1, HIGH); //474
+  digitalWrite(motorB1, HIGH);
   digitalWrite(motorB2, LOW);
 }
 
 void forwardSlow() {
   analogWrite(motorA1, 0);
-  analogWrite(motorA2, 100); // 480
+  analogWrite(motorA2, 100);
   analogWrite(motorB1, 0);
-  analogWrite(motorB2, 100); // 474
+  analogWrite(motorB2, 100);
 
 }
 void backward() {
 
   analogWrite(motorA1, 0);
-  analogWrite(motorA2, 100); // 480
+  analogWrite(motorA2, 100);
   analogWrite(motorB1, 0);
-  analogWrite(motorB2, 100); // 474
+  analogWrite(motorB2, 100);
 
 }
 void stop() {
@@ -287,61 +317,4 @@ void turnAround() {
   analogWrite(motorA2, 0);
   analogWrite(motorB1, 0);
   analogWrite(motorB2, 150);
-}
-
-//void activateGripper(int angle) {
-//  int pulseWidth = map(angle, 0, 180, 0, 255);
-//    digitalWrite(Gripper, HIGH);
-//    Serial.println(pulseWidth);
-//    delayMicroseconds(pulseWidth);
-//    digitalWrite(Gripper, LOW);
-//    delay(20);
-//  }
-
-void activateGripper(int pulse) {
-  for (int i = 0; i < 10; i++)
-  {
-    digitalWrite(Gripper, HIGH);
-    delayMicroseconds(pulse);
-    digitalWrite(Gripper, LOW);
-    delay(20);
-  }
-
-}
-
-//void gripperToggle() {
-//  if (millis() > timer) {
-//    if (state == false) {
-//      gripper(GRIPPER_OPEN);
-//      state = true;
-//    } else {
-//      gripper(GRIPPER_CLOSE);
-//      state = false;
-//    }
-//    timer = millis() + GRIPPER_TOGGLE;
-//  }
-//}
-
-
-int getUltrasonicDistance() {
-  digitalWrite(TrigerPin, LOW);
-  delayMicroseconds(5);
-  digitalWrite(TrigerPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TrigerPin, LOW);
-
-  long duration = pulseIn(EchoPin, HIGH);
-  int distance = duration * 0.034 / 2;
-
-  return distance;
-}
-
-void detec() {
-
-  int distcance = getUltrasonicDistance();
-
-  if (distcance < 25)
-  {
-    check = false;
-  }
 }
